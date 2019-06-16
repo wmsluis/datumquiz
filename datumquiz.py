21078b56742e10724361a9c6e0cc3efdbb331828
willem <wsls@termiet>
2019-06-16 10:59:54 +0200

    Naar python3

--- datumquiz.py
+++ datumquiz.py
@@ -8,14 +8,13 @@ class DatumQuiz(object):
     """toon een datum en laat de gebruiker de dag van de week berekenen"""
 
     def __init__(self, startyear, yearcnt):
-        self.weekdays = ['zo', 'ma', 'di', 'wo', 'do', 'vr', 'za']
+        self.weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
         self.date1 = datetime.date(startyear, 1, 1)
         date2 = datetime.date(startyear + yearcnt - 1, 12, 31)
         self.daycnt = (date2 - self.date1).days
         self.vraag_cnt = 0
         self.correct_cnt = 0
         self.starttijd = datetime.datetime.now()
-        locale.setlocale(locale.LC_ALL, 'nl_NL.utf8')
         
     def play(self):
         # een finite-state machine 
@@ -30,12 +29,12 @@ class DatumQuiz(object):
         d = random.randint(0, self.daycnt)
         datum = self.date1 + datetime.timedelta(days=d)
         self.correct_antwoord = datum.strftime("%a")
-        print "Op welke dag valt %s?" % datum.strftime("%d %b %Y")
+        print("Op welke dag valt %s?" % datum.strftime("%d %b %Y"))
         self.vraag_cnt += 1
         return "get_antwoord"
     
     def get_antwoord(self):
-        invoer = raw_input("(Zon=0, Maa=1 ... Zat=6 of druk op x om te stoppen): ")
+        invoer = input("(Zon=0, Maa=1 ... Zat=6 of druk op x om te stoppen): ")
         if not invoer:
             return "get_antwoord"
 
@@ -54,35 +53,35 @@ class DatumQuiz(object):
                     
         if self.weekdays[antwoord] == self.correct_antwoord:
             self.correct_cnt += 1
-            print "Correct\n"
+            print("Correct\n")
             return "stel_vraag"
         else:
             self.vraag_cnt += 1
-            print "Niet correct, probeer opnieuw ..."
+            print("Niet correct, probeer opnieuw ...")
             return "get_antwoord"
             
     def invoer_incorrect(self):
-        print "Incorrecte invoer, probeer het opnieuw"
+        print("Incorrecte invoer, probeer het opnieuw")
         return "get_antwoord"
     
     def stoppen(self):
         self.vraag_cnt -= 1
         if self.vraag_cnt > 0:
-            print "aantal vragen : %d" % self.vraag_cnt
-            print "correct       : %d" % self.correct_cnt        
+            print("aantal vragen : %d" % self.vraag_cnt)
+            print("correct       : %d" % self.correct_cnt        )
             tijdgebruikt = (datetime.datetime.now() 
                             - self.starttijd).seconds / self.vraag_cnt
-            print "tijd per vraag: %.1f" % tijdgebruikt
+            print("tijd per vraag: %.1f" % tijdgebruikt)
         return "afsluiten" 
 
     def hint (self):
         """toon een hint"""
-        print "weekdag = datum + jaardag - maanddag"
-        print "maanddag:"
-        print "  x/x:    4 april, 6 juni, 8 augustus, 10 october, 12 december"
-        print "  5-to-9: 5 september, 9 mei"
-        print "  7/11:   7 november, 11 juli"
-        print "          2 januari, 6 februari (van het voorliggende jaar)"
-        print "jaardag = eeuwdag + jaar + jaar / 4 (naar beneden afgerond) "
-        print "eeuwdag: 1900=3, 2000=2"
+        print("weekdag = datum + jaardag - maanddag")
+        print("maanddag:")
+        print("  x/x:    4 april, 6 juni, 8 augustus, 10 october, 12 december")
+        print("  5-to-9: 5 september, 9 mei")
+        print("  7/11:   7 november, 11 juli")
+        print("          2 januari, 6 februari (van het voorliggende jaar)")
+        print("jaardag = eeuwdag + jaar + jaar / 4 (naar beneden afgerond) ")
+        print("eeuwdag: 1900=3, 2000=2")
         return "get_antwoord"
--- vind.py
+++ vind.py
@@ -3,9 +3,9 @@
 freq = [0,0,0,0,0,0,0]
 
 for i in range(0,200,10):
-	day = (3 + i + i / 4 ) % 7
-	print 1900 + i, day
+	day = (3 + i + i // 4 ) % 7
+	print(1900 + i, day)
 #		index = (1900 + i) % 7
 	freq[day] = freq[day] + 1
-print freq
+print(freq)
 

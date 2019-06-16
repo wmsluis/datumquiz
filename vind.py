a2a94d70415ce8d11830cbf1cdae29197b0181a2
willem <wsls@termiet>
2019-06-16 13:02:00 +0200

    spelen met fetch en pull

--- vind.py
+++ vind.py
@@ -2,9 +2,9 @@
 
 freq = [0,0,0,0,0,0,0]
 
-# een commentaar toegevoegd
 for i in range(0,200,10):
 	day = (3 + i + i // 4 ) % 7
+# een commentaar toegevoegd
 	print(1900 + i, day)
 	freq[day] = freq[day] + 1
 print(freq)
